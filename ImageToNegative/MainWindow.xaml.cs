c006c09b88b4434dc21f8f3ca554201723c73770
Xoja <Xoja@DESKTOP-457U45V>
2022-05-14 17:05:01 +0500

    Added image mirroring fixed bugs

--- ImageToNegative/MainWindow.xaml.cs
+++ ImageToNegative/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
+using System.Diagnostics;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
@@ -21,6 +22,12 @@ namespace ImageToNegative
         public MainWindow()
         {
             InitializeComponent();
+
+            double width = System.Windows.SystemParameters.WorkArea.Width;
+            double height = System.Windows.SystemParameters.WorkArea.Height;
+            MainWIndow.Height = height;
+            MainWIndow.Width = width;
+
         }
 
         private void miExit_Click(object sender, RoutedEventArgs e)
@@ -75,12 +82,16 @@ namespace ImageToNegative
 
                         switch (type)
                         {
+                            #region Caes Negative
                             case convTypes.Negative:
                                 new_A = a;
                                 new_R = 255 - r;
                                 new_G = 255 - g;
                                 new_B = 255 - b;
                                 break;
+                            #endregion Caes Negative
+
+                            #region Case Binary
                             case convTypes.Binary:
                                 if (avg > 128)
                                 {
@@ -97,27 +108,40 @@ namespace ImageToNegative
                                     new_B = 0;
                                 }
                                 break;
+                            #endregion Case Binary
+
+                            #region Case GrayScaled
                             case convTypes.Grayscaled:
                                 new_A = a;
                                 new_R = avg;
                                 new_G = avg;
                                 new_B = avg;
                                 break;
+                            #endregion Case GrayScaled
+
+                            #region Case Red
                             case convTypes.Red:
                                 new_R = r;
                                 new_G = 0;
                                 new_B = 0;
                                 break;
+                            #endregion Case Red
+
+                            #region case Green
                             case convTypes.Green:
                                 new_R = 0;
                                 new_G = g;
                                 new_B = 0;
                                 break;
+                            #endregion Case Gree 
+
+                            #region Case Blue
                             case convTypes.Blue:
                                 new_R = 0;
                                 new_G = 0;
                                 new_B = b;
                                 break;
+                                #endregion Case Blue
                         }
 
                         bitmap.SetPixel(i, j, Color.FromArgb(a, new_R, new_G, new_B));
@@ -142,6 +166,26 @@ namespace ImageToNegative
                 MessageBox.Show("Please choose image first", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
+
+        private void miSaveAs_Click(object sender, RoutedEventArgs e)
+        {
+            if (changedInstance == null)
+            {
+                MessageBox.Show("Please modify image first", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+                dialog.InitialDirectory = Directory.GetCurrentDirectory();
+                dialog.InitialDirectory = "C:\\Users";
+                dialog.IsFolderPicker = true;
+                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    changedInstance.Save(dialog.FileName + "\\changedImage.png");
+                }
+            }
+        }
+
 
         #region Converting types
         private void miNegative_Click(object sender, RoutedEventArgs e)
@@ -183,6 +227,92 @@ namespace ImageToNegative
         }
         #endregion Converting types
 
+        #region Mirroring 
+        private void MirroringImage(string path, Mirroring side)
+        {
+            try
+            {
+                if (!String.IsNullOrEmpty(path))
+                {
+                    Bitmap orgImg = new Bitmap(path);
+                    int width = orgImg.Width;
+                    int height = orgImg.Height;
+
+                    Bitmap mirImg = new Bitmap(width, height);
+
+
+                    switch (side)
+                    {
+                        case Mirroring.Left:
+                            for (int y = 0; y < height; y++)
+                            {
+                                for (int lx = 0, rx = width - 1; lx < width; lx++, rx--)
+                                {
+                                    Color p = orgImg.GetPixel(lx, y);
+
+                                    mirImg.SetPixel(lx, y, p);
+                                    //mirImg.SetPixel(rx, y, p);
+                                }
+                            }
+                            break;
+
+                        case Mirroring.Right:
+                            for (int y = 0; y < height; y++)
+                            {
+                                for (int lx = 0, rx = width - 1; lx < width; lx++, rx--)
+                                {
+                                    Color p = orgImg.GetPixel(lx, y);
+
+                                    //mirImg.SetPixel(lx, y, p);
+                                    mirImg.SetPixel(rx, y, p);
+                                }
+                            }
+                            break;
+                    }
+                    changedInstance = mirImg;
+
+                    using (MemoryStream memory = new MemoryStream())
+                    {
+                        mirImg.Save(memory, ImageFormat.Png);
+                        memory.Position = 0;
+                        BitmapImage bitmapImage = new BitmapImage();
+                        bitmapImage.BeginInit();
+                        bitmapImage.StreamSource = memory;
+                        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                        bitmapImage.EndInit();
+
+                        imgOriginal.Source = bitmapImage;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Please choose image first", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+
+
+
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e.Message);
+                throw;
+            }
+        }
+
+        private void miRightMirroringClick(object sender, RoutedEventArgs e)
+        {
+            MirroringImage(originalPath, Mirroring.Right);
+        }
+
+        private void miLeftMirroringClick(object sender, RoutedEventArgs e)
+        {
+            MirroringImage(originalPath, Mirroring.Left);
+        }
+
+        #endregion Mirroring
+
+
+        #region Enums 
         enum convTypes
         {
             Binary,
@@ -192,24 +322,15 @@ namespace ImageToNegative
             Green,
             Blue
         }
-
-        private void miSaveAs_Click(object sender, RoutedEventArgs e)
+        enum Mirroring
         {
-            if (changedInstance == null)
-            {
-                MessageBox.Show("Please modify image first", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            else
-            {
-                CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-                dialog.InitialDirectory = Directory.GetCurrentDirectory();
-                dialog.InitialDirectory = "C:\\Users";
-                dialog.IsFolderPicker = true;
-                if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
-                {
-                    changedInstance.Save(dialog.FileName + "\\changedImage.png");
-                }
-            }
+            Right,
+            Left
         }
+
+
+        #endregion Enums
+
+
     }
 }
